0d58a301f61733689a6a7d55f79a49d016a81163
周毅 <yc07444@um.edu.mo>
2020-11-04 22:04:39 +0800

    添加 3.5 中的 vec3 类功能函数。

--- vec3.hpp
+++ vec3.hpp
@@ -22,9 +22,9 @@ public:
     double& operator[](int i) {return e[i];}
 
     vec3& operator+=(const vec3& v) {
-        e[0]+=v.e[0];
-        e[1]+=v.e[1];
-        e[2]+=v.e[2];
+        e[0]+=v[0];
+        e[1]+=v[1];
+        e[2]+=v[2];
         return *this;
     }
 
@@ -55,5 +55,49 @@ private:
 // Type aliases for vec3
 using point3 = vec3; // 3D point
 using color  = vec3; // RGB color
+
+// vec3 Utility Functions
+using std::ostream;
+inline ostream& operator<<(ostream& out, const vec3& v) {
+    return out<<v[0]<<" "<<v[1]<<" "<<v[2];
+}
+
+inline vec3 operator+(const vec3& u, const vec3& v) {
+    return vec3(u[0]+v[0], u[1]+v[1], u[2]+v[2]);
+}
+
+inline vec3 operator-(const vec3& u, const vec3& v) {
+    return vec3(u[0]-v[0], u[1]-v[1], u[2]-v[2]);
+}
+
+inline vec3 operator*(const vec3& u, const vec3& v) {
+    return vec3(u[0]*v[0], u[1]*v[1], u[2]*v[2]);
+}
+
+inline vec3 operator*(double t, const vec3& u) {
+    return vec3(u[0]*t, u[1]*t, u[2]*t);
+}
+
+inline vec3 operator*(const vec3& u, double t) {
+    return t*u;
+}
+
+inline vec3 operator/(const vec3& u, double t) {
+    return (1/t)*u;
+}
+
+//向量点乘
+inline double dot(const vec3& u, const vec3& v) {
+    return u[0]*v[0]+u[1]*v[1]+u[2]*v[2];
+}
+
+//向量叉乘
+inline vec3 cross(const vec3& u, const vec3& v) {
+    return vec3(u[1]*v[2]-u[2]*v[1], u[2]*v[0]-u[0]*v[2], u[0]*v[1]-u[1]*v[0]);
+}
+
+inline vec3 unite_vector(const vec3& v) {
+    return v/v.length();
+}
 
 #endif //end of head file
